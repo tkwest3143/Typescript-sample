48ab7eb16777d2461155f6e04ce20023951e9e7a
Nishi Takanori <47804655+tkwest3143@users.noreply.github.com>
2023-06-16 08:04:30 +0900

    add:input mike

--- ai_chat_flow/src/app/chat/page.tsx
+++ ai_chat_flow/src/app/chat/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 type Message = {
   userId: number;
@@ -8,13 +8,36 @@ type Message = {
 export default function Home() {
   const [messages, setMessages] = useState<Message[]>([]);
   const [input, setInput] = useState("");
+  const [isRecording, setIsRecording] = useState(false);
+  const [isSending, setIsSending] = useState(false);
+  const startRecognition = () => {
+    const SpeechRecognition =
+      (window as any).SpeechRecognition ||
+      (window as any).webkitSpeechRecognition;
+    const recognition = new SpeechRecognition();
 
-  const sendMessage = (e: { preventDefault: () => void }) => {
+    recognition.onstart = () => {
+      setIsRecording(true);
+    };
+
+    recognition.onresult = (event: any) => {
+      const current = event.resultIndex;
+      const transcript = event.results[current][0].transcript;
+      setInput(transcript);
+      setIsRecording(false);
+    };
+
+    recognition.start();
+  };
+
+  const sendMessage = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     if (input !== "") {
       messages.push({ userId: 0, text: input });
       setMessages([...messages]);
+      setIsSending(true);
+
       fetch(`/api/gpt`, {
         method: "POST",
         headers: {
@@ -30,6 +53,7 @@ export default function Home() {
           text: result.result || "sorry not response gpt",
         });
         setMessages([...messages]);
+        setIsSending(false);
       });
       setInput("");
     }
@@ -38,7 +62,7 @@ export default function Home() {
   return (
     <div className="flex flex-col h-screen bg-gray-100">
       <div className="flex flex-row justify-center items-center bg-green-500 p-4">
-        <h1 className="text-white text-2xl">Next.js Chat App</h1>
+        <h1 className="text-white text-2xl">AI Chat Flow</h1>
       </div>
       <div className="flex flex-col flex-grow overflow-y-scroll bg-white">
         {messages.map((message, index) => (
@@ -57,6 +81,13 @@ export default function Home() {
             </p>
           </div>
         ))}
+        {isSending ? (
+          <div className={`flex flex-row justify-start items-end p-4`}>
+            <div className="h-2 w-2 bg-gray-500 rounded-full animate-pulse m-4"></div>
+            <div className="h-2 w-2 bg-gray-500 rounded-full animate-pulse m-4"></div>
+            <div className="h-2 w-2 bg-gray-500 rounded-full animate-pulse m-4"></div>
+          </div>
+        ) : undefined}
       </div>
       <form
         onSubmit={sendMessage}
@@ -67,10 +98,32 @@ export default function Home() {
           onChange={(e) => setInput(e.target.value)}
           placeholder="Type a message"
           className="flex-grow rounded-full p-2 mr-4 bg-white"
-        />
+        />{" "}
+        <button
+          type="button"
+          onClick={startRecognition}
+          className={`bg-blue-500 text-white rounded-full p-2 mr-4 ${
+            isRecording ? "animate-pulse" : ""
+          }`}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className={`h-6 w-6 ${isRecording ? "text-red-500" : "text-white"}`}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 18.75a6 6 0 006-6v-1.5m-6 7.5a6 6 0 01-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 01-3-3V4.5a3 3 0 116 0v8.25a3 3 0 01-3 3z"
+            />
+          </svg>
+        </button>
         <button
           type="submit"
-          className="bg-green-500 text-white rounded-full p-2"
+          className={`bg-green-500 text-white rounded-full p-2 `}
         >
           Send
         </button>
